c8a99502095a99d741dc842fea8fb6c69246dfb7
Malachi Bowman <malachiabowman@gmail.com>
2020-11-24 11:33:14 -0600

    added icons, home page, mobile nav, noty

--- Gemfile
+++ Gemfile
@@ -29,8 +29,10 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 #devise
 gem 'devise'
-#bulma rails
+#bulma 
 gem "bulma-rails", "~> 0.9.1"
+#material icons
+gem 'material_icons'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,30 @@ require("channels")
 
 require("jquery")
 
+window.Noty = require("noty")
+
+//toggle navbar burger
+document.addEventListener("turbolinks:load", function() {
+    $('.navbar-burger').click(function() {
+        $('#navbarBasicExample, .navbar-burger').toggleClass('is-active');
+    });
+    $('.toggle').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        $('#' + e.target.getAttribute('aria-controls')).toggleClass('is-hidden');
+    })
+
+    //toggle modal
+    $('.toggle-modal').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var type = e.target.getAttribute('aria-control');
+        $('#' + type).toggleClass('is-active');
+    });
+})
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
